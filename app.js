220bb454f782c2f17154ea9d74d46dc8f680f23f
spielmeisterg <ianspeelman@gmail.com>
2022-01-18 17:08:44 +0100

    trying to get backend working

--- app.js
+++ app.js
@@ -8,7 +8,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/', (req, res) => {
-    res.sendFile(__dirname + "/public/index.html")
+    res.sendFile("/public/index.html", { root: __dirname })
 });
 
 app.post('/api/world', (req, res) => {
