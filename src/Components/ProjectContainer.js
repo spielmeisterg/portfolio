324dac46937eb24154eb13b9e867de1149683e7f
spielmeisterg <ianspeelman@gmail.com>
2022-02-02 19:45:27 +0100

    added some fonts

--- src/Components/ProjectContainer.js
+++ src/Components/ProjectContainer.js
@@ -9,6 +9,7 @@ class ProjectContainer extends Component{
             <div className="project-container">
                 <div className="project-list">
                     <Project name={projects[0].name} description={projects[0].description} link={projects[0].link} image={projects[0].image} />
+                    <Project name={projects[1].name} description={projects[1].description} link={projects[1].link} image={projects[1].image} />
                 </div>
             </div>
         )
--- src/Components/data.js
+++ src/Components/data.js
@@ -1,16 +1,20 @@
-export const amArray = ["Ian Speelman", "a Developer", "awesome", "the best"]
+export const amArray = [
+    "Ian Speelman",
+    "a Developer",
+    "Awesome",
+    "The best"]
 
 export const projects = [
     {
         name: "project one",
-        description: "this is an awesome description of this projects",
-        image: "https://via.placeholder.com/350x150",
+        description: "Lorem ipsum doillut in ccing elit, sed do . Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.",
+        image: "https://via.placeholder.com/1920x1080/eee?text=16:9",
         link: "link to production server"
     },
     {
         name: "project two",
         description: "this is an awesome description of this projects",
-        image: "https://via.placeholder.com/350x150",
+        image: "https://via.placeholder.com/1920x1080/eee?text=16:9",
         link: "link to production server"
     },
 ]
